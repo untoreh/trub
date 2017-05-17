9094b9f91a3ac8dd0c7ab30d5e0cdb73bd2d8aec
untoreh <bassobassista@gmail.com>
2017-05-17 06:18:17 +0200

    .

--- prepare.sh
+++ prepare.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-cat <<EOF > /etc/apk/repositories
+cat << EOF >/etc/apk/repositories
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
 http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
-apk add --update-cache \
-    bash \
-	wget \
-	git \
-	unzip \
-	xz \
-	coreutils \
-	binutils \
-	libressl \
-	ca-certificates \
-	ostree
+apk add --update-cache  \
+ bash  \
+ wget  \
+ git  \
+ unzip  \
+ tar  \
+ xz  \
+ coreutils  \
+ binutils  \
+ libressl  \
+ ca-certificates  \
+ ostree
 
 source ./functions.sh
 source ./glib.sh
