91c0b9ed7fb1d0548012e37d218a0427c1db8a0c
untoreh <bassobassista@gmail.com>
2017-05-17 06:14:58 +0200

    .

--- build-update.sh
+++ build-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source functions.sh
 
 remote_repo="untoreh/trub"
@@ -9,22 +10,25 @@ tree="tree"
 ref="trunk"
 
 ## confirm we made a new tree
-if [ ! "`find tree/* -maxdepth 0 | wc -l`" -gt 0 ] ; then
+if [ ! "`find tree/* -maxdepth 0 | wc -l`" -gt 0 ]; then
     echo "newer tree not grown. (tree folder is empty)"
     exit 1
 fi
 
 install_tools "ostree util-linux wget"
 
-fetch_artifact $repo $artifact prev
+got=$(fetch_artifact $repo $artifact prev)
 
-cmt=$(b64name prev)
+## if we have an artifact
+if [ $got ]; then
+    cmt=$(b64name prev)
 
-## apply the scratch delta to the empty repo
-rev=$(ostree --repo=$repo static-delta apply-offline ${cmt})
-## name the commit
-ostree --repo=$repo refs --delete $ref
-ostree --repo=$repo refs --create=$ref $rev
+    ## apply the scratch delta to the empty repo
+    rev=$(ostree --repo=$repo static-delta apply-offline ${cmt})
+    ## name the commit
+    ostree --repo=$repo refs --delete $ref
+    ostree --repo=$repo refs --create=$ref $rev
+fi
 
 ## commit the recently grown tree on top of the previous image in the repo
 newrev=$(ostree --repo=$repo commit -s $(date)'-build' -b $ref --tree=dir=${tree})
@@ -33,9 +37,9 @@ newrev=$(ostree --repo=$repo commit -s $(date)'-build' -b $ref --tree=dir=${tree
 ostree prune --repo=$repo --refs-only --keep-younger-than="3 months ago"
 
 ## then generate the sparse and scratch delta and archive
-ostree --repo=${repo} static-delta generate $ref --inline --min-fallback-size 0 \
-    --filename=${newrev}
+ostree --repo=${repo} static-delta generate $ref --inline --min-fallback-size 0  \
+ --filename=${newrev}
 tar cf $delta_artifact $newrev && rm $newrev
-ostree --repo=${repo} static-delta generate $ref --inline --min-fallback-size 0 \
-    --filename=${newrev} --empty
+ostree --repo=${repo} static-delta generate $ref --inline --min-fallback-size 0  \
+ --filename=${newrev} --empty
 tar cf $newrev && rm $newrev
