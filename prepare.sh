4d0b6a32102ba634568812a9fa2544f774d64a35
untoreh <bassobassista@gmail.com>
2017-05-17 05:55:17 +0200

    .

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+cat <<EOF > /etc/apk/repositories
+http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
+http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+EOF
+
 apk add --update-cache \
     bash \
 	wget \
