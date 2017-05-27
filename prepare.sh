45d9abac09850f5fb65707e6bb302ef8ca54fedc
untoreh <bassobassista@gmail.com>
2017-05-27 16:07:45 +0200

    updated repositories

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-cat << EOF >/etc/apk/repositories
-http://dl-cdn.alpinelinux.org/alpine/latest-stable/main
-http://dl-cdn.alpinelinux.org/alpine/latest-stable/community
-http://dl-cdn.alpinelinux.org/alpine/edge/community
+cat << EOF >>/etc/apk/repositories
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
